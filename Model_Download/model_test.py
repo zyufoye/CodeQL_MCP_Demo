2267aa1c2ade4277cf422021ff14ae098f277c21
zyufoye <zyufoye@gmail.com>
2025-11-26 11:10:30 +0800

    Model Download and Test

--- Model_Download/model_test.py
+++ Model_Download/model_test.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_path = r"C:\Users\Aono\Desktop\Project\CodeQL_MCP_Test\deepseek-coder-1.3b"  # 你下载的目录
+model_path = r"C:\Users\Aono\Desktop\Project\CodeQL_MCP_Demo\deepseek-coder-1.3b"  # 你下载的目录
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path)
